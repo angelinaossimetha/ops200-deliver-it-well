be534211c2858b363bbdcc439820f9650d2e5a49
aossimetha <angelina@cccareers.org>
2021-10-26 13:32:46 -0400

    Change <h2>

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -37,4 +37,16 @@ describe('server/app.js', function() {
       done();
     });
   });
+
+  it('page says hola world', (done) => {
+    chai.request(server)
+      .get('/')
+      .end((err, res) => {
+        expect(err).not.exist;
+        expect(JSON.stringify(res.text)).to.contain('Hola World');
+        done();
+      });
+    });
 })
+
+
